e2c58528fc9b6f7b9cc233c96319d6a294ccd997
AmirKhakiDev <amirkhaki593@gmail.com>
2025-12-11 10:12:22 +0330

    some change

--- django_web_app/blog/api/v1/serializer.py
+++ django_web_app/blog/api/v1/serializer.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "email"]
+        fields = ["username", "email"]
 
 
 class PostSerializer(ModelSerializer):
